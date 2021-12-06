5e306564f6ab00782596c798f2de4e86203282aa
Nathália Ramos <88409702+NathyNickii@users.noreply.github.com>
2021-12-05 23:11:41 -0300

    Add files via upload

---
+++ aritmeticos.js
@@ -0,0 +1,20 @@
+var soma = 10 + 10
+console.log (soma)
+
+var soma = 9 - 8
+console.log (soma)
+
+var soma = 10 + 10
+console.log (soma)
+
+var soma = 10 + 10
+console.log (soma)
+
+var soma = 25 % 3
+console.log (soma)
+
+var x = 10
+var y = 30
+
+var getResultado = x + y
+console.log("RESULTADO DA SOMA: ", getResultado)
